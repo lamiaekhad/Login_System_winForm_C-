4ed66fc3a05495599a32846a0264964a6113790d
lamiaekhad <102177275+lamiaekhad@users.noreply.github.com>
2022-08-10 13:00:02 -0400

    modification BD

--- Security_Project_BD.sql
+++ Security_Project_BD.sql
@@ -5,8 +5,8 @@ prenom varchar(25) NOT NULL,
 nomutilisateur varchar(25) not null,
 courriel varchar(30) not null,
 motdepasse varchar(500) not null,
-confirmation varchar(500) not null,
 statut varchar(20)not null,
+thecount int not null,
 primary Key (id)
 );
 
